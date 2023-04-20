27b8a39bf196999160285b7a4390e641b78bdf69
alfredo <alfredoaarc11@gmail.com>
2023-04-20 13:13:51 +0200

    junit5

--- TeamMicroServiciesGFT/src/test/java/microservicesgft/teammicroservicesgft/CartServiceTest.java
+++ TeamMicroServiciesGFT/src/test/java/microservicesgft/teammicroservicesgft/CartServiceTest.java
@@ -5,7 +5,6 @@ import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
